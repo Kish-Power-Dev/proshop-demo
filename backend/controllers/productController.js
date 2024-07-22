74f6df7cdbe006eed687ef9e0f7700756b0ebf48
Kishore <kishorekishu171@gmail.com>
2024-07-23 01:45:54 +0530

    Backend : Create Reviews

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -81,7 +81,46 @@ const deleteProduct = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Product deleted" });
   } else {
     res.status(404);
-    throw new Error("Reource not found");
+    throw new Error("Resource not found");
+  }
+});
+
+// @desc    Create a new review
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (review) => review.user.toString() === req.user._id.toString()
+    );
+
+    if (alreadyReviewed) {
+      res.status(400);
+      throw new Error("Product already reviewed");
+    }
+
+    const review = {
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+      user: req.user._id,
+    };
+
+    product.reviews.push(review);
+
+    product.rating =
+      product.reviews.reduce((acc, review) => acc + review.rating, 0) /
+      product.reviews.length;
+
+    await product.save();
+    res.status(201).json({ message: "Review addes" });
+  } else {
+    res.status(404);
+    throw new Error("Resource not found");
   }
 });
 
@@ -91,4 +130,5 @@ export {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 };
